b5dfa79c4f1d361e9ecc5ab41fda582258e4b7ae
sivaprakasham <37078127+sivaprakasham@users.noreply.github.com>
2018-03-06 20:34:15 +0530

    Update mozimage.php

--- mozimage.php
+++ mozimage.php
@@ -1,6 +1,5 @@
 <html>
 <body>
-<img src="https://raw.githubusercontent.com/mdn/beginner-html-site/gh-pages/images/firefox-icon.png">
 <p>A link to my <a href="https://www.mozilla.org/" title="The Mozilla homepage" target="_blank">favourite website</a>.</p>
 </body>
 </html>
